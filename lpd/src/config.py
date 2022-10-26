d6e4aaf47549abf54b5139916bd4a61e42326731
jwang307 <john.wang1726@gmail.com>
2022-10-25 20:45:01 -0700

    separated individual piano tracks

--- lpd/src/config.py
+++
@@ -1,7 +0,0 @@
-"""Define configuration variables
-"""
-CONFIG = {
-    'multicore': 40, # the number of cores to use (1 to disable multiprocessing)
-    'beat_resolution': 24, # temporal resolution (in time step per beat)
-    'time_signatures': ['4/4'] # '3/4', '2/4'
-}
--- npz_file.py
+++ npz_file.py
@@ -3,18 +3,28 @@ import os
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
-    folder = '/Users/jwang/MusicDiffusion/lpd/lpd_cleansed/A/A/'
-    output = 'test'
+    root_dir = os.getcwd()
+    folder = f'{root_dir}/lpd_5/lpd_5_cleansed/A/'
+    output_audio = 'piano_audios'
+    output_npz = 'piano_tracks'
+    count = 0
     for root, folders, files in os.walk(folder):
         path = root
         for file in files:
+            count += 1
             if file.endswith('.npz'):
                 multitrack = pp.load(f'{root}/{file}')
                 for track in multitrack:
                     # a midi code from 0-127 to denote what instrument to select
-                    if track.pianoroll.sum() != 0:
-                        new_track = pp.Multitrack(tracks=[track])
-                        new_track.trim()
-                        pp.write(f'/Users/jwang/MusicDiffusion/{output}/{multitrack.name}_{str(track.program)}.mid', new_track)
+                    if track.pianoroll.sum() != 0 and track.name == 'Piano':
+                        new_track = pp.Multitrack(tracks=[track.trim()])
+                        if not os.path.exists(f'{root_dir}/{output_audio}/'):
+                            os.mkdir(f'{root_dir}/{output_audio}/')
+                        if not os.path.exists(f'{root_dir}/{output_npz}/'):
+                            os.mkdir(f'{root_dir}/{output_npz}/')
+
+                        pp.write(f'{root_dir}/{output_audio}/{multitrack.name}.mid', new_track)
+                        pp.save(f'{root_dir}/{output_npz}/{multitrack.name}.npz', new_track)
                         # track.plot()
     # plt.show()
+    print(count)
